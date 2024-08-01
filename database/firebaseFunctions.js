01bb893541cbe2188fc42118f465926db5842446
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-07-31 20:48:02 -0400

    removed Auth functions

--- database/firebaseFunctions.js
+++ database/firebaseFunctions.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import StartingPokemonTeam from "../utilities/StartingPokemonTeam.js";
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { getFirestore, setDoc, doc, updateDoc, collection, getDocs } from "firebase/firestore";
+import { setDoc, doc, updateDoc, collection, getDocs, initializeFirestore } from "firebase/firestore";
 
 
 const testUid = "qwertyuiopasdfghjkl"
@@ -23,81 +22,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-const db = getFirestore(app)
+const db = initializeFirestore(app, { experimentalForceLongPolling: true })
 
-const signUp = (username, email, password) => {
-    if(isUsernameAvailable()){
-        createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            if(typeof window != "undefined"){
-                localStorage.setItem('uid',user.uid)
-                localStorage.setItem('username',username)
-            }
-            // then create the pokedex in the database
-            createCollectionForUserId(user.uid, username)
-        })
-        .catch((error) => {
-            console.error(error.code)
-            console.error(error.message)    
-        });
-        return true // signUp successfull
-    }
-    return false // username not available
-}
-
-const signIn = async(email, password) => {
-    let uid
-    signInWithEmailAndPassword(auth, email, password)
-    .then(credential => {
-        uid = credential.user.uid
-        if(typeof window != "undefined"){
-            localStorage.setItem("uid",uid)
-        }    
-    })
-    .catch(error => {
-        console.error(error.code)
-        console.error(error.message)
-    })
-    try{
-        const querySnapshot = await getDocs(collection(db, 'pokedex'))
-        querySnapshot.forEach(document => {
-            if(document.id == uid){
-                const username = document.data().username
-                if(typeof window != "undefined"){
-                    localStorage.setItem("username",username)
-                }
-            }
-        })
-    }
-    catch(e){
-        console.error(e)
-    }
-    return true
-}
-
-// TODO: refactor function
-const isUsernameAvailable = async(username) => {
-    let isAvailable = true
-    let user = username.toLowerCase().trim()
-    await db()
-        .collection("users")
-        // .where("user","==",user)
-        .get()
-        .then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-                if(doc?.data()?.user.length > 0 && doc?.data()?.user.toLowerCase() === user){
-                    isAvailable = false
-                }
-            })
-        })
-        .catch(e => {
-            console.error(e)
-            isAvailable = false
-        })
-    return isAvailable
-}
 
 const createCollectionForUserId = async(uid = testUid, username = testUsername) => {
     const initialPokedex = StartingPokemonTeam
@@ -155,4 +81,4 @@ const updateCollectionForUserId = async(uid = testUid, collection) => {
     }
 }
 
-export { signIn, signUp, createCollectionForUserId, getCollectionForUserId, updateCollectionForUserId }
+export { createCollectionForUserId, getCollectionForUserId, updateCollectionForUserId }
