43115b4a00b5b12e6b99792ad5961b7ba413dbbe
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-08-08 19:54:42 -0400

    fixed hpBar style to reduce to the chosen side

--- components/BattleComponents/HpBar/HpBar.jsx
+++ components/BattleComponents/HpBar/HpBar.jsx
@@ -5,12 +5,28 @@ import HpBarStyle from "./HpBarStyle";
 const HpBar = ({stats, reduceToLeft = true}) => {
 
     const remainingHpPercentage = stats.Hp / stats.MaxHp * 100
+    const marginToAdd = 100 - remainingHpPercentage
 
     return(
         <>
         <View style={HpBarStyle.hpWrapper}>
-            <View style={{...HpBarStyle.hpBackground, justifyContent: reduceToLeft ? 'flex-end' : 'flex-start'}}>
-                <View style={{...HpBarStyle.hpFill ,width: `${remainingHpPercentage}%`}}></View>
+            <View style={HpBarStyle.hpBackground}>
+                {
+                    reduceToLeft &&
+                    <View style={{
+                        ...HpBarStyle.hpFill, 
+                        width: `${remainingHpPercentage}%`,
+                        marginRight: `${marginToAdd}%`
+                        }}></View>
+                }
+                {
+                    !reduceToLeft &&
+                    <View style={{
+                        ...HpBarStyle.hpFill, 
+                        width: `${remainingHpPercentage}%`,
+                        marginLeft: `${marginToAdd}%`
+                        }}></View>
+                }
             </View>
         </View>
         </>
