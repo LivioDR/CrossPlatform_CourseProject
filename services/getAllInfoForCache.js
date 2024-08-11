aea72ccf09faaab5b653355b87bb13792bee5387
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-08-11 13:16:58 -0400

    added flag to cache storage

--- services/getAllInfoForCache.js
+++ services/getAllInfoForCache.js
@@ -6,7 +6,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const atkBaseUrl = "https://pokeapi.co/api/v2/move/"
 
 const getAllInfoForCache = async() => {
-
     // set to true if want to display the progress on console
     const displayProgressInConsole = true
 
@@ -23,28 +22,37 @@ const getAllInfoForCache = async() => {
         backend: AsyncStorage,
     })
 
-    try{
-        if(displayProgressInConsole){
-            setProgress(0)
-        }
-        await getAllPokemon()
-        if(displayProgressInConsole){
-            setProgress(10)
-        }
-        for(let i=1; i<=919; i++){
-            let move = await cache.get(`${atkBaseUrl}${i}/`)
+    // checks if the data was previously populated into the cache to skip requesting it all over again
+    const isCompleted = await cache.get("completed")
 
-            if(!move){
-                move = await fetch(`${atkBaseUrl}${i}/`).then(res => res.json())
-                await cache.set(`${atkBaseUrl}${i}/`,move)
-            }
+    if(!isCompleted){
+        try{
             if(displayProgressInConsole){
-                setProgress(10 + ((i/919) * 90))
+                setProgress(0)
             }
+            await getAllPokemon()
+            if(displayProgressInConsole){
+                setProgress(10)
+            }
+            for(let i=1; i<=919; i++){
+                let move = await cache.get(`${atkBaseUrl}${i}/`)
+    
+                if(!move){
+                    move = await fetch(`${atkBaseUrl}${i}/`).then(res => res.json())
+                    await cache.set(`${atkBaseUrl}${i}/`,move)
+                }
+                if(displayProgressInConsole){
+                    setProgress(10 + ((i/919) * 90))
+                }
+            }
+            // setting the flag on the cache once the data has been stored
+            await cache.set("completed",true)
         }
+        catch(e){
+            console.error(e)
+        }
+
     }
-    catch(e){
-        console.error(e)
-    }
+
 }
 export default getAllInfoForCache
